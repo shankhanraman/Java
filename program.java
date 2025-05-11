0b820f4d85d337904d61916da371d1b1662950db
shankhanraman <shankhan.raman@gmail.com>
2025-05-11 19:07:27 +0530

    Fourth commit

---
+++ program.java
@@ -0,0 +1,39 @@
+class SimpleStartup {
+  private int[] locationCells;
+  private int numOfHits = 0;
+
+  public void setLocationCells(int[] locs) {
+    locationCells = locs;
+  }
+
+  public String checkYourself(int guess) {
+    String result = "miss";
+    for (int cell : locationCells) {
+      if (guess == cell) {
+        result = "hit";
+        numOfHits++;
+        break;
+      } // end if
+    } // end for
+    if (numOfHits ==
+        locationCells.length) {
+      result = "kill";
+    } // end if
+    System.out.println(result);
+    return result;
+  } // end method
+} // close class
+public class program {
+     public static void main(String[] args) {
+    SimpleStartup dot = new SimpleStartup();
+    int[] locations = {2, 3, 4};
+    dot.setLocationCells(locations);
+    int userGuess = 2;
+    String result = dot.checkYourself(userGuess);
+    String testResult = "failed";
+    if (result.equals("hit")) {
+      testResult = "passed";
+    }
+    System.out.println(testResult);
+  }
+}
